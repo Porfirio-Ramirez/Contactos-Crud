72b2539da6f8cba521987184c8e5dce15a4df445
Porfirio Ramirez <Porfirioramirez999@gmail.com>
2025-11-24 07:00:04 -0400

    Validaciones

--- Contactos-crud.Api/Models/Contacto.cs
+++ Contactos-crud.Api/Models/Contacto.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Contactos_crud.Api.Models
 {
     public class Contacto
     {
-       public int Id {get; set;} 
+
+        public int Id { get; set; }
+
+       [Required(ErrorMessage = "El nombre es obligatorio")]
        public string Nombre {get; set;}
+       [Required(ErrorMessage = "El teléfono es obligatorio")]
        public string Telefono {get; set;}
+       [Required(ErrorMessage = "El correo es obligatorio")]
        public string Correo {get; set;}
       
     }
