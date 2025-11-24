ae453fe27065b129cec837857542252d12d4c2b7
Porfirio Ramirez <Porfirioramirez999@gmail.com>
2025-11-23 20:32:23 -0400

    Agregar modelo contacto

---
+++ Contactos-crud.Api/Models/Contacto.cs
@@ -0,0 +1,11 @@
+namespace Contactos_crud.Api.Models
+{
+    public class Contacto
+    {
+       public int Id {get; set;} 
+       public string Nombre {get; set;}
+       public string Telefono {get; set;}
+       public string Correo {get; set;}
+      
+    }
+}
